232832f89ee00f1fd55c8103020df595f71c0122
aa00518 <jbanach33@gmail.com>
2017-12-14 10:17:11 -0600

    First commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { TodolistComponent } from './components/todolist/todolist.component';
 
+import { TodolistService } from './services/todolist.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [TodolistService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
